64407d0778fba3a4a873c09abb20a07374b4971d
Steve Unixson <unixson@gmail.com>
2018-05-15 23:13:01 +0300

    бэмс и никакого дерьма на трусах

--- main.js
+++ main.js
@@ -1,79 +1,50 @@
-"use strict";
+'use strict';
+const electron = require('electron');
 
-const electron = require('electron')
-const app = electron.app
-const globalShortcut = electron.globalShortcut
-const os = require('os')
-const path = require('path')
-const request = require('request');
-const querystring = require('querystring');
-const {ipcMain} = require('electron')
-const BrowserWindow = electron.BrowserWindow
+const app = electron.app;
 
-//TODO add config
-//const config = require('config.json')('./config/app.json');
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
 
+// prevent window being garbage collected
+let mainWindow;
 
-require('dotenv').config();
-
-var mainWindow = null;
-
-//app.setName(config.productName)
-
-function windowCreate() {
-  mainWindow = new BrowserWindow({
-    width: 1000, 
-    height: 1000,
-    show: true,
-    webPreferences: {
-      nodeIntegration: true,
-      defaultEncoding: 'UTF-8'
-    }
-  });
-  if (process.env.PACKAGE === 'true'){
-    mainWindow.loadURL(`file://${__dirname}/app/login/index.html`);
-    //mainWindow.setMenu(null);
-
-  } else {
-   mainWindow.loadURL(`file://${__dirname}/app/login/index.html`);
-   mainWindow.webContents.openDevTools();
-  }
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
 }
 
-function login() {
-  
-  var form = {
-    username: 'usr',
-    password: 'pwd',
-    opaque: 'opaque',
-    logintype: '1'
-};
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 1280,
+		height: 840
+	});
 
-var formData = querystring.stringify(form);
-var contentLength = formData.length;
+	win.loadURL(`file://${__dirname}/app/login/index.html`);
+	//win.setMenu(null);
+	win.on('closed', onClosed);
 
-request({
-    headers: {
-      'Content-Length': contentLength,
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'x-api-key': 'testkey'
-    },
-    uri: 'http://localhost:8001/login',
-    body: formData,
-    method: 'POST'
-  }, function (err, res, body) {
-    //it works!
-  });
+	return win;
 }
 
-
-
-app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-        app.quit();
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-app.on('ready', windowCreate);
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
+
+
 
 
 
